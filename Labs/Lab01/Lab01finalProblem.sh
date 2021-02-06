a25b6cc4a9eed7d5e53c28df6c04126cd293a450
msbrasher <mabr7162@colorado.edu>
2021-02-06 14:50:40 -0700

    added comments to Lab01 script

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,6 +1,14 @@
 ## Lab01 Script
 
+# Remove Row Names
+
 cut -f 2-4 -d , PredPreyData.csv > cutPredPrey.csv
+
+# View last 10 lines
+
 tail cutPredPrey.csv
+
+# Put last 10 lines and header row into a new file
+
 grep time cutPredPrey.csv > endPredPrey.csv
 tail cutPredPrey.csv >> endPredPrey.csv
